cc142748bfcfafb8fcba6cc4ae77b7e14ad8bef2
Alejandro Fuentes Cleto <fuentescletoalejandro@gmail.com>
2021-04-22 12:25:52 -0500

    turn change, pair founded events of the game implemented

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,9 @@ import React from 'react'
 import "../styles/card.css";
 import PropTypes from "prop-types";
 
-const Card = ({ imageUrl, id, flipped, handleSelectCard, pairID }) => {
+const Card = ({ imageUrl, id, flipped, handleSelectCard, pairID, founded }) => {
     return (
-        <div onClick={() => handleSelectCard(id)} className={`card ${flipped ? "flipped" : ""}`}>
+        <div onClick={() => handleSelectCard(id)} className={`card ${flipped || founded ? "flipped" : ""}`}>
             <div className="overlay"></div>
             <img src={imageUrl} alt="" />
         </div>
@@ -16,7 +16,8 @@ Card.propTypes = {
     flipped: PropTypes.bool.isRequired,
     id: PropTypes.string.isRequired,
     handleSelectCard: PropTypes.func.isRequired,
-    pairID: PropTypes.string.isRequired
+    pairID: PropTypes.string.isRequired,
+    founded: PropTypes.bool.isRequired
 }
 
 
